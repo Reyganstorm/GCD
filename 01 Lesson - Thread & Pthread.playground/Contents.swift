69c6d838df17e7a6a335ed351df0f6aa0160ef1a
Reyganstorm <ruslan.shtybaev@gmail.com>
2022-10-11 22:15:06 +0300

    create 1 lesson

--- 01 Lesson - Thread & Pthread.playground/Contents.swift
+++ 01 Lesson - Thread & Pthread.playground/Contents.swift
@@ -2,7 +2,33 @@
 // Operation
 // GCD
 
+import Darwin
+import UIKit
+
 
 // паралельная
 // последовательная
 // асинхронная
+
+// Unix - POSIX
+
+
+var thread = pthread_t(bitPattern: 0 ) // создаем поток
+var attribit = pthread_attr_t() // создаем атрибут
+
+pthread_attr_init(&attribit)
+
+pthread_create(&thread, &attribit, { pointer in
+    print("test 1")
+    return nil
+}, nil)
+
+// 2 Thread
+
+var nstread = Thread {
+    print("test 2")
+}
+
+nstread.start()
+nstread.cancel()
+nstread.isFinished
